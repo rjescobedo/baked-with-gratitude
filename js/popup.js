282617fb377c064a4e5da04da2a2148e4e88e231
Raul Escobedo <rjescobedo@gmail.com>
2025-01-03 06:21:27 -0700

    edits to the popup and calendar

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const popup = document.getElementById("popup");
     const closePopup = document.getElementById("closePopup");
     const popupButton = document.getElementById("popupBtn");
+    const pickupDate = document.getElementById("pickup-date");
 
     // Show the popup when the page loads
     setTimeout(() => {
@@ -25,5 +26,19 @@ document.addEventListener("DOMContentLoaded", () => {
             popup.style.display = "none";
         }
     });
+
+    // Update the pickup date to every Friday until the end of the year
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    const currentMonth = today.getMonth();
+    const currentDay = today.getDate();
+    const currentDayOfWeek = today.getDay();
+    const daysUntilFriday = 5 - currentDayOfWeek;
+    const nextFriday = new Date(currentYear, currentMonth, currentDay + daysUntilFriday);
+    
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const nextFridayFormatted = nextFriday.toLocaleDateString('en-US', options);
+    
+    pickupDate.innerHTML += `<br>${nextFridayFormatted}!`;
 
 });
